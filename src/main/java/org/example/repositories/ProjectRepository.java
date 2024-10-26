a8c1328ac23848c8d0082827b5d5f7c4327e112c
Vada <denis.le01@mail.ru>
2024-10-26 20:42:30 +0300

    task-5 drop repositories

--- src/main/java/org/example/repositories/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repositories;
-
-import org.example.models.Project;
-
-public class ProjectRepository extends Repository<Project> {
-    public ProjectRepository() {
-        super(Project.class);
-    }
-}
--- src/main/java/org/example/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repositories;
-
-import org.example.models.User;
-
-public class UserRepository extends Repository<User>{
-    public UserRepository() {
-        super(User.class);
-    }
-}
