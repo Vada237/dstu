614e7f4f57683bd72bb65c37e26d2ce6ef6190f2
Vada <denis.le01@mail.ru>
2024-10-27 12:57:08 +0300

    task-4 set tmp solution

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -1,12 +1,15 @@
 package org.example;
 
-import org.example.repositories.UserRepository;
+import org.example.models.Task;
+import org.example.models.User;
+import org.example.models.UserTask;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) throws SQLException, NoSuchFieldException, IllegalAccessException {
-        UserRepository repository = new UserRepository();
-        System.out.println(repository.getAll());
+        UserTask.all();
+        System.out.println(1);
     }
 }
