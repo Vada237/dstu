46deec97ebea894a92cdd633396a909d70fb0255
Denis Le <denis.le@softezza.ru>
2024-10-29 17:50:45 +0300

    Task-6 update interface

--- src/main/java/org/example/services/userTask/UserTaskWebService.java
+++ src/main/java/org/example/services/userTask/UserTaskWebService.java
@@ -5,7 +5,6 @@ import org.example.models.Task;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import java.util.concurrent.Future;
 import java.sql.SQLException;
 
 @WebService
@@ -13,12 +12,4 @@ import java.sql.SQLException;
 public interface UserTaskWebService {
     @WebMethod
     public void trackTime(Task task, int time, int progress) throws SQLException;
-    @WebMethod
-    public void checkProgress(Task task) throws SQLException;
-    @WebMethod
-    public Future<Void> addProgress(Task task, int progress) throws SQLException;
-    @WebMethod
-    public Future<Void> addPivotProgress(Task task, int time, int progress) throws SQLException;
-    @WebMethod
-    public void updateStatus(Task task) throws SQLException;
 }
